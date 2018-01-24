580868b8f4617bcf0eef19951876258dfa0c9fcf
Cam Nguyen <cnguyen1187@gmail.com>
2018-01-24 06:08:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ You should NOT try to redefine any of the above variables.
 // Write your code below this line!
 const newID ='';
 
-const randomNumber = ()>=0 & <20);
+const randomNumber = >=0 & <20;
 Math.floor(Math.random() * 10) + 11;
